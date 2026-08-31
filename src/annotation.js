e73cf07fd3d28dc07602dc5e7ce759fe930ebc66
t-akatsuki <t-akatsuki@utakata-no-yume.net>
2026-08-31 18:02:43 +0900

    add: jsファイルのテンプレートを追加。
    - ESModuleで開発し、ビルド時にIIFE形式に変換する。
    - アノテーション部分を分離して必ず先頭に付与するように工夫。

---
+++ src/annotation.js
@@ -0,0 +1,19 @@
+//=============================================================================
+// UTA_RPGMVZPluginTemplate
+//=============================================================================
+/*:
+ * @target MZ
+ * @plugindesc Plugin description
+ * @author t-akatsuki
+ * @url Plugin distribution url
+ * @help
+ * Plugin usage detail
+ */
+/*:ja
+ * @target MZ
+ * @plugindesc プラグインの簡単な説明
+ * @author t-akatsuki
+ * @url プラグインの配布元URL
+ * @help
+ * プラグインの詳細な説明文
+ */
---
+++ src/main.js
@@ -0,0 +1,17 @@
+/**
+ * utakata名前空間。
+ * @namespace
+ */
+window.utakata = window.utakata || {};
+
+/**
+ * UTA_PluginTemplate名前空間。
+ * @namespace
+ */
+window.utakata.UTA_PluginTemplate = {
+    /**
+     * プラグインのバージョン。
+     * @type {string}
+     */
+    "VERSION": __VERSION__,
+};
